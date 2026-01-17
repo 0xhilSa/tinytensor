4f0961f5e62816e6968e5c361c39cfbfdc9fe9c5
Sahil <sahilrajwar2004@gmail.com>
2026-01-18 01:25:33 +0530

    testing

--- test.py
+++ test.py
@@ -1,12 +1,18 @@
 import tinytensor as tt
 
-x = tt.Tensor([[1,2,3,4],[5,6,7,8]], dtype=tt.dtypes.int8, device="cpu", const=True)
-y = tt.Tensor([[1,2,3,4],[5,6,7,8]], dtype=tt.dtypes.int8, device="cuda", const=True)
-print("=====on CPU=====")
-print(x)
+x = tt.Tensor([[1,2,3,4],[5,6,7,8]], dtype=tt.dtypes.float64, device="cpu:0")
+y = tt.Tensor([[1,2,3,4],[5,6,7,8]], dtype=tt.dtypes.float64, device="cuda:0")
+
+print("=====(tensor at CPU initially)=====")
 print(x.buf)
-print(x.device)
-print("=====on CUDA=====")
-print(y)
+print(x)
+print(x.cuda())
+print("=====(tensor at CUDA initially)=====")
 print(y.buf)
-print(y.device)
+print(y)
+print(y.cpu())
+
+print(tt.gpu_cuda.device_count())
+print(tt.gpu_cuda.device_name(0))
+print(tt.gpu_cuda.device_prop(0))
+print(tt.gpu_cuda.is_available())
