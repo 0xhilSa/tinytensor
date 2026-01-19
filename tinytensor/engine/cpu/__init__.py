5e83b861b71dd1190166da4be8099c7eb6739966
Sahil <sahilrajwar2004@gmail.com>
2026-01-20 02:04:32 +0530

    update: frontend for cpu_ops

--- tinytensor/engine/cpu/__init__.py
+++ tinytensor/engine/cpu/__init__.py
@@ -1,8 +1,9 @@
 from tinytensor.engine.cpu.cpu import tocpu, tolist
-from tinytensor.engine.cpu.cpu_ops import add
+from tinytensor.engine.cpu.cpu_ops import add, sub
 
 __all__ = [
   "tocpu",
   "tolist",
-  "add"
+  "add",
+  "sub"
 ]
