7fb77bdf9819d34c6c9d82d7fc40978c0d9e7d05
Sahil <sahilrajwar2004@gmail.com>
2026-01-20 01:24:20 +0530

    new: link cpu_ops and cpu to the __init__.py

--- tinytensor/engine/cpu/__init__.py
+++ tinytensor/engine/cpu/__init__.py
@@ -1,4 +1,8 @@
 from tinytensor.engine.cpu.cpu import tocpu, tolist
 from tinytensor.engine.cpu.cpu_ops import add
 
-__all__ = ["tocpu", "tolist", "add"]
+__all__ = [
+  "tocpu",
+  "tolist",
+  "add"
+]
