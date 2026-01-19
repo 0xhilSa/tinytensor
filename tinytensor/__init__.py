1db14574b0a6bcea690d940c3e40972dce7994d0
Sahil <sahilrajwar2004@gmail.com>
2026-01-20 01:25:01 +0530

    new: __add__ operation added

--- tinytensor/__init__.py
+++ tinytensor/__init__.py
@@ -1,4 +1,4 @@
-import ctypes
+from __future__ import annotations
 from typing import List, Optional, Union
 from tinytensor import dtypes
 from tinytensor.shape import Shape
@@ -10,23 +10,38 @@ from tinytensor.device import Device
 class Tensor:
   def __init__(
     self,
-    buf:Union[List,ctypes.c_void_p],
+    buf:Union[List,dtypes.ConstType],
     dtype:Optional[Union[dtypes.DType,dtypes.ConstType]]=None,
     device:str="cpu",
     const:bool=False
   ):
-    if isinstance(buf,List):
-      self.shape = Shape(infer_shape(buf))
-      buf = flatten(buf)
-      buf, self.dtype = infer_dtype(buf, dtype)
-      self.device = Device(device)
-      self.const = const
-      if self.device.type == "CPU": self.buf = cpu.tocpu(buf, self.shape.shape, self.dtype.fmt)
-      elif self.device.type == "CUDA": self.buf =cuda.tocuda(buf, self.shape.shape, self.dtype.fmt, self.device.index)
-    else: raise NotImplementedError("create tensor from the pointer has not been implemented yet!") # not planned yet
+    if isinstance(buf, dtypes.ConstType): buf = [buf]
+    self.shape = Shape(infer_shape(buf))
+    buf = flatten(buf)
+    buf, self.dtype = infer_dtype(buf, dtype)
+    self.device = Device(device)
+    self.const = const
+    if self.device.type == "CPU": self.buf = cpu.tocpu(buf, self.shape.shape, self.dtype.fmt)
+    elif self.device.type == "CUDA": self.buf =cuda.tocuda(buf, self.shape.shape, self.dtype.fmt, self.device.index)
   def __repr__(self): return f"Tensor(shape={self.shape.shape}, dtype='{self.dtype.ctype}', device='{self.device.type}:{self.device.index}', const={self.const})"
   def cpu(self): return Tensor(reshape(cpu.tolist(cuda.tocpu(self.buf)), self.shape.shape), dtype=self.dtype) if self.device.type == "CUDA" else self
   def cuda(self): return Tensor(reshape(cpu.tolist(self.buf), self.shape.shape), dtype=self.dtype, device="cuda:0") if self.device.type == "CPU" else self
+  def tolist(self): return reshape(cpu.tolist(self.buf), self.shape.shape) if self.device.is_cpu() else reshape(cpu.tolist(cuda.tocpu(self.buf)), self.shape.shape)
+  def __add__(self, other:Union[Tensor,dtypes.ConstType]): # prototype
+    if isinstance(other, dtypes.ConstType): other = Tensor(other, dtype=self.dtype, device=f"{self.device.type}:{self.device.index}")
+    elif isinstance(other, Tensor):
+      assert other.device == self.device, "addition cannot be perform on tensors with different devices"
+      assert other.dtype == self.dtype, "addition cannot be perform on tensors with different dtypes"
+    if isinstance(other, dtypes.ConstType): other = Tensor(other, dtype=self.dtype, device=f"{self.device.type}:{self.device.index}")
+    if self.device.is_cpu(): return Tensor(reshape(cpu.tolist(cpu.add(self.buf, other.buf)), self.shape.shape), dtype=self.dtype)
+    raise NotImplementedError("__add__ not implemented for tensors on CUDA device")
+  def __radd__(self, other:Union[Tensor,dtypes.ConstType]): # prototype
+    if isinstance(other, dtypes.ConstType): other = Tensor(other, dtype=self.dtype, device=f"{self.device.type}:{self.device.index}")
+    elif isinstance(other, Tensor):
+      assert other.device == self.device, "addition cannot be perform on tensors with different devices"
+      assert other.dtype == self.dtype, "addition cannot be perform on tensors with different dtypes"
+    if self.device.is_cpu(): return Tensor(reshape(cpu.tolist(cpu.add(self.buf, other.buf)), self.shape.shape), dtype=self.dtype)
+    raise NotImplementedError("__add__ not implemented for tensors on CUDA device")
 
 
 def tensor(
