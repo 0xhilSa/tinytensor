1b04ea3cb387fb091ad27428109ffbfa5533caa9
Sahil <sahilrajwar2004@gmail.com>
2026-01-21 01:54:42 +0530

    update: get_device

--- tinytensor/engine/cuda/__init__.py
+++ tinytensor/engine/cuda/__init__.py
@@ -1,11 +1,11 @@
-from tinytensor.engine.cuda.cuda import device_count, device_prop, device_name, is_available, tocpu, tocuda
+from tinytensor.engine.cuda.cuda import device_count, device_prop, device_name, is_available, get_device, tocpu, tocuda
 
 __all__ = [
   "device_count",
   "device_prop",
   "device_name",
   "is_available",
-  "is_available",
+  "get_device",
   "tocpu",
   "tocuda"
 ]
