c89560f9d4837be4233ed1582d748fc37f02f6ad
Sahil <sahilrajwar2004@gmail.com>
2026-01-23 13:53:01 +0530

    update: driver/runtime version and driver package added

--- tinytensor/engine/cuda/__init__.py
+++ tinytensor/engine/cuda/__init__.py
@@ -1,6 +1,9 @@
-from tinytensor.engine.cuda.cuda import device_count, device_prop, device_name, is_available, get_device, tocpu, tocuda
+from tinytensor.engine.cuda.cuda import device_count, device_prop, device_name, is_available, get_device, tocpu, tocuda, runtime_version, driver_version, driver_package
 
 __all__ = [
+  "driver_package",
+  "driver_version",
+  "runtime_version",
   "device_count",
   "device_prop",
   "device_name",
