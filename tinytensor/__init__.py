926768789f7537263419575ad25dbe4df8eb831f
Sahil <sahilrajwar2004@gmail.com>
2026-02-01 23:09:57 +0530

    import more stuff when init

--- tinytensor/__init__.py
+++ tinytensor/__init__.py
@@ -1,4 +1,3 @@
-from __future__ import annotations
 from typing import List, Optional, Union
 from tinytensor.tensor import Tensor
 from tinytensor.engine import cpu, cuda
@@ -9,11 +8,16 @@ from tinytensor.device import Device
 def cuda_available(): return hasattr(cuda, "device_count") and cuda.device_count() > 0
 
 def tensor(
-  buf:List,
+  buf:Union[List[dtypes.ConstType],dtypes.ConstType],
   dtype:Optional[Union[dtypes.DType,dtypes.ConstType]]=None,
   device:str="cpu",
-  const:bool=False
+  const:bool=False # type: ignore (type interference)
 ): return Tensor(buf=buf, dtype=dtype, device=device, const=const)
+
+from tinytensor.dtypes import (bool, int8, uint8, int16, uint16,
+                               int32, uint32, int64, uint64,
+                               float32, float64, float128,
+                               complex64, complex128, complex256)
 
 __all__ = [
   "cuda_available",
@@ -22,5 +26,20 @@ __all__ = [
   "Device",
   "dtypes",
   "cpu",
-  "cuda"
+  "cuda",
+  "bool",
+  "int8",
+  "uint8",
+  "int16",
+  "uint16",
+  "int32",
+  "uint32",
+  "int64",
+  "uint64",
+  "float32",
+  "float64",
+  "float128",
+  "complex64",
+  "complex128",
+  "complex256"
 ]
