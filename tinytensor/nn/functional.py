632638d0a058d992e43b456400a922b20e6082d2
Sahil Rajwar <justsahilrajwar2004@gmail.com>
2026-02-15 13:18:46 +0530

    update: activation functions

--- tinytensor/nn/functional.py
+++ tinytensor/nn/functional.py
@@ -1,17 +1,21 @@
+from tinytensor import dtypes
 from tinytensor.engine.cpu import functional_cpu as F_c
+from tinytensor.engine.cuda import functional_cuda as F_cu
 from tinytensor.tensor import Tensor
 
 def relu(x:Tensor, requires_grad:bool=False, const:bool=False) -> Tensor:
-  out = F_c.relu(x.buf)
+  out = F_c.relu(x.buf) if x.device.type == "CPU" else F_cu.relu(x.buf)
   return Tensor._from_view(out, dtype=x.dtype, device=x.device, requires_grad=requires_grad, const=const)
 
 def gelu(x:Tensor, requires_grad:bool=False, const:bool=False) -> Tensor:
-  out = F_c.gelu(x.buf)
-  return Tensor._from_view(out, dtype=x.dtype, device=x.device, requires_grad=requires_grad, const=const)
+  out = F_c.gelu(x.buf) if x.device.type == "CPU" else F_cu.gelu(x.buf)
+  dtype = dtypes.float32 if x.dtype == dtypes.float32 else dtypes.float64
+  return Tensor._from_view(out, dtype=dtype, device=x.device, requires_grad=requires_grad, const=const)
 
-def leaky_relu(x:Tensor, alpha:float=0.01, requires_grad:bool=False, const:bool=False) -> Tensor:
-  out = F_c.leaky_relu(x.buf, alpha)
-  return Tensor._from_view(out, dtype=x.dtype, device=x.device, requires_grad=requires_grad, const=const)
+def leaky_relu(x:Tensor, negative_slope:float=0.01, requires_grad:bool=False, const:bool=False) -> Tensor:
+  out = F_c.leaky_relu(x.buf, negative_slope) if x.device.type == "CPU" else F_cu.leaky_relu(x.buf, negative_slope)
+  dtype = dtypes.float32 if x.dtype == dtypes.float32 else dtypes.float64
+  return Tensor._from_view(out, dtype=dtype, device=x.device, requires_grad=requires_grad, const=const)
 
 __all__ = [
   "relu",
