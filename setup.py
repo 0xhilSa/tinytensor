5df58f6c5088c8708f1f2f6a3c603166d185c090
Sahil Rajwar <sahilrajwar2004@gmail.com>
2026-02-15 09:25:58 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -4,4 +4,18 @@ setup(
   name="tinytensor",
   version="0.2.0",
   packages=find_packages(),
+  include_package_data=True,
+  package_data={
+    "tinytensor": [
+      "engine/**/*.so",
+    ]
+  },
+  python_requires=">=3.8",
+  author="Sahil Rajwar",
+  url="https://github.com/0xhilSa/tinytensor",
+  license="MIT",
+  classifiers=[
+    "License :: OSI Approved :: MIT License",
+  ],
+  description="A lightweight tensor computation library",
 )
