5b6e896b621f48cdb4c7ab1b16dfe2d720f86b6f
Sahil Rajwar <justsahilrajwar2004@gmail.com>
2026-02-15 13:19:18 +0530

    compile functionals module .c & .cu

--- compile.sh
+++ compile.sh
@@ -32,7 +32,7 @@ CU_OUT_DIR=$CU_SRC_DIR
 
 # compile ./tinytensor/engine/cpu/cpu.c
 echo "compiling $C_SRC_DIR/cpu.c -> $C_SRC_DIR/cpu.so"
-run_with_spinner nvcc -gencode arch=compute_75,code=sm_75 -O3 -Xcompiler -fPIC -shared \
+run_with_spinner nvcc -gencode arch=compute_86,code=sm_86 -O3 -Xcompiler -fPIC -shared \
   -I"$PY_INC" \
   -I"$C_SRC_DIR" \
   "$C_SRC_DIR/cpu.c" \
@@ -42,14 +42,14 @@ run_with_spinner nvcc -gencode arch=compute_75,code=sm_75 -O3 -Xcompiler -fPIC -
 
 # compile ./tinytensor/engine/constants.c
 echo "compiling $TEN_SRC/constants.c -> $TEN_SRC/constants.so"
-run_with_spinner nvcc -gencode arch=compute_75,code=sm_75 -O3 -Xcompiler -fPIC -shared \
+run_with_spinner nvcc -gencode arch=compute_86,code=sm_86 -O3 -Xcompiler -fPIC -shared \
   -I"$PY_INC" \
   "$TEN_SRC/constants.c" \
   -o "$TEN_SRC/constants.so"
 
 # compile ./tinytensor/engine/cpu/cpu_ops.c
 echo "compiling $C_SRC_DIR/cpu_ops.c -> $C_SRC_DIR/cpu_ops.so"
-run_with_spinner nvcc -gencode arch=compute_75,code=sm_75 -O3 -Xcompiler -fPIC -shared \
+run_with_spinner nvcc -gencode arch=compute_86,code=sm_86 -O3 -Xcompiler -fPIC -shared \
   -I"$PY_INC" \
   -I"$C_SRC_DIR" \
   "$C_SRC_DIR/cpu_ops.c" \
@@ -59,7 +59,7 @@ run_with_spinner nvcc -gencode arch=compute_75,code=sm_75 -O3 -Xcompiler -fPIC -
 
 # compile ./tinytensor/engine/cuda/cuda.cu
 echo "compiling $CU_SRC_DIR/cuda.cu -> $CU_SRC_DIR/cuda.so"
-run_with_spinner nvcc -gencode arch=compute_75,code=sm_75 -O3 -Xcompiler -fPIC -shared \
+run_with_spinner nvcc -gencode arch=compute_86,code=sm_86 -O3 -Xcompiler -fPIC -shared \
   -I"$PY_INC" \
   -I"$CU_SRC_DIR" \
   "$CU_SRC_DIR/cuda.cu" \
@@ -69,14 +69,30 @@ run_with_spinner nvcc -gencode arch=compute_75,code=sm_75 -O3 -Xcompiler -fPIC -
 
 # compile ./tinytensor/engine/cuda/cuda_ops.cu
 echo "compiling $CU_SRC_DIR/cuda_ops.cu -> $CU_SRC_DIR/cuda_ops.so"
-run_with_spinner nvcc -gencode arch=compute_86,code=sm_86 \
-  -gencode arch=compute_86,code=compute_86 \
-  -O3 -Xcompiler -fPIC -shared \
+run_with_spinner nvcc -gencode arch=compute_86,code=sm_86 -O3 -Xcompiler -fPIC -shared \
   -I"$PY_INC" \
   -I"$CU_SRC_DIR" \
   "$CU_SRC_DIR/cuda_ops.cu" \
   "$TEN_SRC/tensor.c" \
   -o "$CU_OUT_DIR/cuda_ops.so"
+
+# compile ./tinytensor/engine/cuda/functional_cuda.cu
+echo "compiling $CU_SRC_DIR/functional_cuda.cu -> $CU_SRC_DIR/functional_cuda.so"
+run_with_spinner nvcc -gencode arch=compute_86,code=sm_86 -O3 -Xcompiler -fPIC -shared \
+  -I"$PY_INC" \
+  -I"$CU_SRC_DIR" \
+  "$CU_SRC_DIR/functional_cuda.cu" \
+  "$TEN_SRC/tensor.c" \
+  -o "$CU_OUT_DIR/functional_cuda.so"
+
+# compile ./tinytensor/engine/cpu/functional_cpu.c
+echo "compiling $C_SRC_DIR/functional_cpu.cu -> $C_SRC_DIR/functional_cpu.so"
+run_with_spinner nvcc -gencode arch=compute_86,code=sm_86 -O3 -Xcompiler -fPIC -shared \
+  -I"$PY_INC" \
+  -I"$C_SRC_DIR" \
+  "$C_SRC_DIR/functional_cpu.c" \
+  "$TEN_SRC/tensor.c" \
+  -o "$C_OUT_DIR/functional_cpu.so"
 
 pip install -r ./requirements.txt
 pip install -e .
