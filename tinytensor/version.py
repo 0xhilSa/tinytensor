2e4aa7c0661c1ac0ee6ab88cf85110bf3731f78d
Sahil Rajwar <justsahilrajwar2004@gmail.com>
2026-02-24 04:22:03 +0530

    versions

---
+++ tinytensor/version.py
@@ -0,0 +1,6 @@
+from typing import Optional
+
+__all__ = ["__version__", "cuda", "git_version"]
+__version__ = "0.3.0+cu131"
+cuda: Optional[str] = "13.1"
+git_version = "13141b79c2a4701872a95da76644723d595b0b6a"
