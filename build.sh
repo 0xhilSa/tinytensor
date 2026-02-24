56aab081128ee50b1674146b9150292bba01aa2c
Sahil Rajwar <justsahilrajwar2004@gmail.com>
2026-02-24 11:07:37 +0530

    build tinytensor(for distribution)

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
+#!/usr/bin/bash
+
+VERSION="0.3.0"
+GIT_HASH=$(git rev-parse HEAD 2>/dev/null || echo "unknow")
+CUDA_VERSION=$(nvcc --version | grep release | sed 's/.*release //' | sed 's/,.*//')
+VERSION="${VERSION}+cu${CUDA_VERSION//./}"
+
 cat > tinytensor/version.py << EOF
 # auto generted
 from typing import Optional
