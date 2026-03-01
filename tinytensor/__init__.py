46b4d1c502d77cd8dc64f49decff10295a78751e
Sahil Rajwar <justsahilrajwar2004@gmail.com>
2026-03-02 00:32:10 +0530

    feat: eye, bugs fixed

--- tinytensor/__init__.py
+++ tinytensor/__init__.py
@@ -1,11 +1,12 @@
 from functools import reduce
-from typing import List, Optional, Union, Tuple
+from typing import List, Optional, Union
 from tinytensor import dtypes
+from tinytensor.engine.cpu.cpu import dtype
 from tinytensor.tensor import Tensor
 from tinytensor.engine import cpu, cuda
 from tinytensor.engine.constants import pi, e, euler_gamma, inf, nan
 from tinytensor.helpers import reshape
-from tinytensor.device import Device
+from tinytensor.device import Device, DeviceLike
 from tinytensor import version
 from tinytensor.dtypes import (bool, int8, uint8, int16, uint16,
                                int32, uint32, int64, uint64,
@@ -20,18 +21,17 @@ def cuda_available(): return hasattr(cuda, "device_count") and cuda.device_count
 def tensor(
   buf:Union[List[dtypes.ConstType],dtypes.ConstType],
   dtype:Optional[Union[dtypes.DType,dtypes.ConstType]]=None,
-  device:str="cpu",
-  requires_grad:bool=False, # type: ignore (type interference)
-  const:bool=False # type: ignore (type interference)
+  device:Union[str,Device,None]=None,
+  requires_grad:bool=False, # type: ignore[valid-type]
+  const:bool=False # type: ignore[valid-type]
 ): return Tensor(buf=buf, dtype=dtype, device=device, requires_grad=requires_grad, const=const)
-
 
 def arange(
   start:int=0, stop:int=0, step:int=1,
   dtype:Union[dtypes.ConstType,dtypes.DType]=int64,
-  device:Union[str,Device]="cpu",
-  requires_grad:bool=False, # type: ignore
-  const:bool=False # type: ignore
+  device:Union[str,Device,None]=None,
+  requires_grad:bool=False, # type: ignore[valid-type]
+  const:bool=False # type: ignore[valid-type]
 ):
   if step == 0: raise ValueError("step parameter cannot be 0")
   return Tensor(list(range(start, stop, step)), dtype=dtype, device=device, requires_grad=requires_grad, const=const)
@@ -41,8 +41,8 @@ def linspace(
   *,
   dtype:Optional[dtypes.DType]=None,
   device:Union[str,Device]="cpu",
-  requires_grad:bool=False, # type: ignore
-  const:bool=False # type: ignore
+  requires_grad:bool=False, # type: ignore[valid-type]
+  const:bool=False # type: ignore[valid-type]
 ) -> Tensor:
   if steps <= 0: raise ValueError("linspace steps must be a positive integer")
   if dtype is None:
@@ -59,45 +59,45 @@ def ones(
   *shape,
   dtype:Union[dtypes.DType,dtypes.ConstType]=dtypes.float32,
   device:Union[str,Device]="cpu",
-  requires_grad:bool=False, # type: ignore
-  const:bool=False # type: ignore
+  requires_grad:bool=False, # type: ignore[valid-type]
+  const:bool=False # type: ignore[valid-type]
 ) -> Tensor:
   if len(shape) == 1 and isinstance(shape, (list, tuple)): shape = shape[0]
   length = 1
   for x in shape: length *= x
-  return Tensor(reshape([1 for _ in range(length)], shape), dtype=dtype, device=device, requires_grad=requires_grad, const=const)
+  return Tensor(reshape([1] * length, shape), dtype=dtype, device=device, requires_grad=requires_grad, const=const)
 
 def zeros(
   *shape,
   dtype:Union[dtypes.DType,dtypes.ConstType]=dtypes.float32,
   device:Union[str,Device]="cpu",
-  requires_grad:bool=False, # type: ignore
-  const:bool=False # type: ignore
+  requires_grad:bool=False, # type: ignore[valid-type]
+  const:bool=False # type: ignore[valid-type]
 ) -> Tensor:
   if len(shape) == 1 and isinstance(shape, (list, tuple)): shape = shape[0]
   length = 1
   for x in shape: length *= x
-  return Tensor(reshape([0 for _ in range(length)], shape), dtype=dtype, device=device, requires_grad=requires_grad, const=const)
+  return Tensor(reshape([0] * length, shape), dtype=dtype, device=device, requires_grad=requires_grad, const=const)
 
 def fill(
   value,
   *shape,
   dtype:Union[dtypes.DType,dtypes.ConstType]=dtypes.float32,
   device:Union[str,Device]="cpu",
-  requires_grad:bool=False, # type: ignore
-  const:bool=False # type: ignore
+  requires_grad:bool=False, # type: ignore[valid-type]
+  const:bool=False # type: ignore[valid-type]
 ) -> Tensor:
   if len(shape) == 1 and isinstance(shape, (list, tuple)): shape = shape[0]
   length = 1
   for x in shape: length *= x
-  return Tensor(reshape([value for _ in range(length)], shape), dtype=dtype, device=device, requires_grad=requires_grad, const=const)
+  return Tensor(reshape([value] * length, shape), dtype=dtype, device=device, requires_grad=requires_grad, const=const)
 
 def empty(
   *shape,
   dtype:dtypes.DType=dtypes.float32,
   device:Union[str,Device]="cpu",
-  requires_grad:bool=False, # type: ignore
-  const:bool=False # type: ignore
+  requires_grad:bool=False, # type: ignore[valid-type]
+  const:bool=False # type: ignore[valid-type]
 ) -> Tensor:
   if len(shape) == 1 and isinstance(shape, (list, tuple)): shape = shape[0]
   if not isinstance(shape, tuple): raise TypeError("shape must be a tuple")
@@ -105,9 +105,16 @@ def empty(
   for dim in shape:
     if not isinstance(dim, int): raise TypeError("shape elements must be int")
     if dim < 0: raise ValueError("negative dimensions not allowed")
-  device = Device(device) if not isinstance(device, Device) else device
+  if not isinstance(device, Device) or device is None: device = Device(device)
   caps = cpu.empty(shape, dtype.fmt)
   return Tensor._from_backend(caps, dtype=dtype, device=device, requires_grad=requires_grad, const=const)
+
+@staticmethod
+def eye(m:int, n:int|None=None, dtype:dtypes.DType=dtypes.float32, device:Union[str,Device]="cpu", requires_grad:bool=False, const:bool=False): # type: ignore
+  if n is None: n = m
+  if not isinstance(device, Device): device = Device(device)
+  buf = cpu.eye(m, n, dtype.fmt) if device.type == "CPU" else cuda.eye(m, n, dtype.fmt)
+  return Tensor._from_backend(buf, dtype=dtype, device=device, requires_grad=requires_grad, const=const)
 
 def __ensure_tensor(x):
   if isinstance(x,Tensor): return x
@@ -119,11 +126,12 @@ def __binop(op, *args:Union[Tensor,List,dtypes.ConstType], res_dtype:dtypes.DTyp
   tensors = [__ensure_tensor(a) for a in args]
   return reduce(op, tensors)
 
-def __unary_backend(cpu_op, cuda_op, x):
+def __unary_backend(cpu_op, cuda_op, x, requires_grad=False, const=False):
   x = __ensure_tensor(x)
   if x.device.type == "CPU": out_buf = cpu_op(x.buf)
   else: out_buf = cuda_op(x.buf)
-  return Tensor._from_backend(out_buf, dtype=x.dtype, device=x.device)
+  requires_grad = x.requires_grad or requires_grad
+  return Tensor._from_backend(out_buf, dtype=dtypes.DType.from_ctype(dtype(out_buf)), device=x.device, requires_grad=requires_grad, const=const)
 
 def add(*args:Union[Tensor,List,dtypes.ConstType]) -> Tensor: return __binop(lambda x,y: x + y, *args)
 def sub(*args:Union[Tensor,List,dtypes.ConstType]) -> Tensor: return __binop(lambda x,y: x - y, *args)
@@ -186,21 +194,21 @@ def logical_xnor(x:Union[Tensor,List,dtypes.ConstType], y:Union[Tensor,List,dtyp
   out = cpu.logical_xnor(x.buf, y.buf) if x.device.type == "CPU" else cuda.logical_xnor(x.buf, y.buf)
   return Tensor._from_backend(out, dtype=dtypes.bool, device=x.device)
 
-def exp(x:Union[Tensor,List,dtypes.ConstType]) -> Tensor: return __unary_backend(cpu.exp, cuda.exp, x)
-def log(x:Union[Tensor,List,dtypes.ConstType]) -> Tensor: return __unary_backend(cpu.log, cuda.log, x)
-def log2(x:Union[Tensor,List,dtypes.ConstType]) -> Tensor: return __unary_backend(cpu.log2, cuda.log2, x)
-def log10(x:Union[Tensor,List,dtypes.ConstType]) -> Tensor: return __unary_backend(cpu.log10, cuda.log10, x)
-def sin(x:Union[Tensor,List,dtypes.ConstType]) -> Tensor: return __unary_backend(cpu.sin, cuda.sin, x)
-def cos(x:Union[Tensor,List,dtypes.ConstType]) -> Tensor: return __unary_backend(cpu.cos, cuda.cos, x)
-def tan(x:Union[Tensor,List,dtypes.ConstType]) -> Tensor: return __unary_backend(cpu.tan, cuda.tan, x)
-def asin(x:Union[Tensor,List,dtypes.ConstType]) -> Tensor: return __unary_backend(cpu.asin, cuda.asin, x)
-def acos(x:Union[Tensor,List,dtypes.ConstType]) -> Tensor: return __unary_backend(cpu.acos, cuda.acos, x)
-def atan(x:Union[Tensor,List,dtypes.ConstType]) -> Tensor: return __unary_backend(cpu.atan, cuda.atan, x)
-def sinh(x:Union[Tensor,List,dtypes.ConstType]) -> Tensor: return __unary_backend(cpu.sinh, cuda.sinh, x)
-def cosh(x:Union[Tensor,List,dtypes.ConstType]) -> Tensor: return __unary_backend(cpu.cosh, cuda.cosh, x)
-def tanh(x:Union[Tensor,List,dtypes.ConstType]) -> Tensor: return __unary_backend(cpu.tanh, cuda.tanh, x)
-def asinh(x:Union[Tensor,List,dtypes.ConstType]) -> Tensor: return __unary_backend(cpu.asinh, cuda.asinh, x)
-def acosh(x:Union[Tensor,List,dtypes.ConstType]) -> Tensor: return __unary_backend(cpu.acosh, cuda.acosh, x)
-def atanh(x:Union[Tensor,List,dtypes.ConstType]) -> Tensor: return __unary_backend(cpu.atanh, cuda.atanh, x)
-def sgn(x:Union[Tensor,List,dtypes.ConstType]) -> Tensor: return __unary_backend(cpu.sgn, cuda.sgn, x)
+def exp(x:Union[Tensor,List,dtypes.ConstType], requires_grad:bool=False, const:bool=False) -> Tensor: return __unary_backend(cpu.exp, cuda.exp, x, requires_grad, const) # type: ignore[type-valid]
+def log(x:Union[Tensor,List,dtypes.ConstType], requires_grad:bool=False, const:bool=False) -> Tensor: return __unary_backend(cpu.log, cuda.log, x, requires_grad, const) # type: ignore[type-valid]
+def log2(x:Union[Tensor,List,dtypes.ConstType], requires_grad:bool=False, const:bool=False) -> Tensor: return __unary_backend(cpu.log2, cuda.log2, x, requires_grad, const) # type: ignore[type-valid]
+def log10(x:Union[Tensor,List,dtypes.ConstType], requires_grad:bool=False, const:bool=False) -> Tensor: return __unary_backend(cpu.log10, cuda.log10, x, requires_grad, const) # type: ignore[type-valid]
+def sin(x:Union[Tensor,List,dtypes.ConstType], requires_grad:bool=False, const:bool=False) -> Tensor: return __unary_backend(cpu.sin, cuda.sin, x, requires_grad, const) # type: ignore[type-valid]
+def cos(x:Union[Tensor,List,dtypes.ConstType], requires_grad:bool=False, const:bool=False) -> Tensor: return __unary_backend(cpu.cos, cuda.cos, x, requires_grad, const) # type: ignore[type-valid]
+def tan(x:Union[Tensor,List,dtypes.ConstType], requires_grad:bool=False, const:bool=False) -> Tensor: return __unary_backend(cpu.tan, cuda.tan, x, requires_grad, const) # type: ignore[type-valid]
+def asin(x:Union[Tensor,List,dtypes.ConstType], requires_grad:bool=False, const:bool=False) -> Tensor: return __unary_backend(cpu.asin, cuda.asin, x, requires_grad, const) # type: ignore[type-valid]
+def acos(x:Union[Tensor,List,dtypes.ConstType], requires_grad:bool=False, const:bool=False) -> Tensor: return __unary_backend(cpu.acos, cuda.acos, x, requires_grad, const) # type: ignore[type-valid]
+def atan(x:Union[Tensor,List,dtypes.ConstType], requires_grad:bool=False, const:bool=False) -> Tensor: return __unary_backend(cpu.atan, cuda.atan, x, requires_grad, const) # type: ignore[type-valid]
+def sinh(x:Union[Tensor,List,dtypes.ConstType], requires_grad:bool=False, const:bool=False) -> Tensor: return __unary_backend(cpu.sinh, cuda.sinh, x, requires_grad, const) # type: ignore[type-valid]
+def cosh(x:Union[Tensor,List,dtypes.ConstType], requires_grad:bool=False, const:bool=False) -> Tensor: return __unary_backend(cpu.cosh, cuda.cosh, x, requires_grad, const) # type: ignore[type-valid]
+def tanh(x:Union[Tensor,List,dtypes.ConstType], requires_grad:bool=False, const:bool=False) -> Tensor: return __unary_backend(cpu.tanh, cuda.tanh, x, requires_grad, const) # type: ignore[type-valid]
+def asinh(x:Union[Tensor,List,dtypes.ConstType], requires_grad:bool=False, const:bool=False) -> Tensor: return __unary_backend(cpu.asinh, cuda.asinh, x, requires_grad, const) # type: ignore[type-valid]
+def acosh(x:Union[Tensor,List,dtypes.ConstType], requires_grad:bool=False, const:bool=False) -> Tensor: return __unary_backend(cpu.acosh, cuda.acosh, x, requires_grad, const) # type: ignore[type-valid]
+def atanh(x:Union[Tensor,List,dtypes.ConstType], requires_grad:bool=False, const:bool=False) -> Tensor: return __unary_backend(cpu.atanh, cuda.atanh, x, requires_grad, const) # type: ignore[type-valid]
+def sgn(x:Union[Tensor,List,dtypes.ConstType], requires_grad:bool=False, const:bool=False) -> Tensor: return __unary_backend(cpu.sgn, cuda.sgn, x, requires_grad, const) # type: ignore[type-valid]
 
